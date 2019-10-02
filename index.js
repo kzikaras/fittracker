cc4f0002e4498b3108f41060e3fd987dc6439f8a
Kurt Zikaras <kurtzikaras@gmail.com>
2019-10-02 07:02:54 -0400

    in progress

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const Sequelize = require('sequelize');
+const session = require('express-session');
 const port = 3000;
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
+// DB Setup
 const db = new Sequelize('bbhybrid', 'postgres', 'Halothedog123', {
     host: 'localhost',
     dialect: 'postgres',
@@ -64,23 +66,51 @@ db.authenticate()
 // Handlebars middleware
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+app.set('trust proxy', true);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(session({
+    genid: (req) => {
+        return Math.random();
+    },
+    secret: 'otisman',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+        maxAge: 8640
+    }
+}));
+
 
 app.get('/', (req, res) => {
-    res.render('index');
+    console.log(req.session);
+    console.log(req.sessionID);
+    if (req.session.member) {
+        Workout.findAll({ where: { memberId: req.session.member.id } })
+            .then((workouts) => {
+                res.render('dashboard', { workouts: workouts });
+            });
+        res.render('dashboard', workouts = workouts)
+    } else {
+        res.render('index');
+    }
 });
 
 app.post('/login', (req, res) => {
     // TODO: make more robust and add session
+    console.log(req.session);
+    console.log(req.sessionID);
     Member.findOne({ where: { email: req.body.login_email } })
         .then((member) => {
             if (req.body.login_password === member.password) {
+                req.session.member = member;
                 Workout.findAll({ where: { memberId: member.id } })
                     .then((workouts) => {
                         res.render('dashboard', { workouts: workouts });
                     });
             } else {
+                req.session.save();
                 res.render('index');
             }
         }).catch((err) => {
@@ -90,6 +120,8 @@ app.post('/login', (req, res) => {
 
 // TODO verify a user doesnt exist before creating
 app.post('/signup', (req, res) => {
+    console.log(req.session);
+    console.log(req.sessionID);
     return Member.create({
         email: req.body.email,
         username: req.body.username,
@@ -104,6 +136,8 @@ app.post('/signup', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
+    console.log(req.session);
+    console.log(req.sessionID);
     res.render('about');
 });
 
