80cde921bd3ca747246aec2bc18db33fd992827b
Kurt Zikaras <kurtzikaras@gmail.com>
2019-10-25 18:04:30 -0400

    update your profile info

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,4 +1,5 @@
-set_error = (message) => {
+set_error = (message, e) => {
+    e.preventDefault();
     $('#errors').html('<h3>' + message + '</h3>');
     setTimeout(() => {
         $('#errors').html('');
@@ -8,8 +9,14 @@ set_error = (message) => {
 // Validate update profile
 $('#update_profile').click((e) => {
     console.log('clicked');
-    e.preventDefault();
-    $('form[action="/update_profile"] input[type="text"]').each(() => {
-        console.log(this);
-    })
+    if ($('input[name="email"]').val() === '')
+        set_error('Please enter an email', e);
+    if ($('input[name="username"]').val() === '')
+        set_error('Please enter a username', e);
+    if ($('input[name="password"]').val() === '')
+        set_error('Please enter a password', e);
+    if ($('input[name="passwordconfirm"]').val() === '')
+        set_error('Please confirm your password', e);
+    if ($('input[name="password"]').val() !== $('input[name="passwordconfirm"]').val())
+        set_error('Passwords must match', e);
 });
